4787d56bc6ab1a5e093e0f7d62f563b628b75735
zhangcy <zcy0521@gmail.com>
2020-10-19 20:04:16 +0800

    Velocity

--- spring-boot-sample-cache-redis/cache-redis-redis-dao/src/main/java/com/sample/springboot/cache/redis/repository/OrderRepository.java
+++
@@ -1,7 +0,0 @@
-package com.sample.springboot.cache.redis.repository;
-
-import com.sample.springboot.cache.redis.model.Order;
-import org.springframework.data.repository.CrudRepository;
-
-public interface OrderRepository extends CrudRepository<Order, Long> {
-}
--- spring-boot-sample-cache-redis/cache-redis-redis-dao/src/main/java/com/sample/springboot/cache/redis/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.sample.springboot.cache.redis.repository;
-
-import com.sample.springboot.cache.redis.model.User;
-import org.springframework.data.repository.CrudRepository;
-
-public interface UserRepository extends CrudRepository<User, Long> {
-}
